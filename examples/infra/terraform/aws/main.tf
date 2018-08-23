eb65891f4dc0bf5dccd9e489d9f914e0649b4a3c
Michael Paesold <mpaesold@gmail.com>
2018-08-23 08:11:27 +0200

    Add hello world httpd server and ingress to hello-world

--- examples/infra/terraform/aws/main.tf
+++ examples/infra/terraform/aws/main.tf
@@ -8,9 +8,30 @@ resource "aws_instance" "helloworld" {
   ami   = "ami-40d28157"
   count = 1
 
-  instance_type = "t2.micro"
+  instance_type          = "t2.micro"
+  vpc_security_group_ids = [" ${aws_security_group.instance.id}"]
+
+  user_data = <<-EOF
+                #!/bin/bash
+                echo "Hello World!" > index.html
+                nohup busybox httpd -f -p 8080 &
+                EOF
 
   tags {
     Name = "terraform-helloworld"
+  }
+}
+
+resource "aws_security_group" "instance" {
+  name = "terraform-helloworld-instance"
+
+  ingress {
+    from_port = 80
+
+    to_port = 8080
+
+    protocol = "tcp"
+
+    cidr_blocks = ["0.0.0.0/0"]
   }
 }
