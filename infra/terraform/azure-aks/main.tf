85d599b1a914deb058be510916d3d0b4c800937b
Michael Paesold <michael.paesold@nttdata.com>
2018-08-31 00:29:22 +0200

    Add service principal with access to the AKS resource group

--- infra/terraform/azure-aks/main.tf
+++ infra/terraform/azure-aks/main.tf
@@ -18,3 +18,29 @@ resource "azurerm_resource_group" "aks_rg" {
     environment = "aks"
   }
 }
+
+resource "azurerm_azuread_application" "aks_sp_app" {
+  name = "cloudgroundaks"
+  homepage = "http://cloudgroundaks"
+  identifier_uris = [
+    "http://cloudgroundaks"]
+  available_to_other_tenants = false
+  oauth2_allow_implicit_flow = false
+}
+
+resource "azurerm_azuread_service_principal" "aks_sp" {
+  application_id = "${azurerm_azuread_application.aks_sp_app.application_id}"
+}
+
+resource "azurerm_role_assignment" "aks_sp_assignment" {
+  /** Scope assigned to created resource group only: */
+  scope = "${azurerm_resource_group.aks_rg.id}"
+  /** Alternative scope: whole subscription. */
+  # scope = "${data.azurerm_subscription.primary.id}"
+
+  role_definition_name = "Contributor"
+
+  principal_id = "${azurerm_azuread_service_principal.aks_sp.id}"
+}
+
+data "azurerm_subscription" "primary" {}
