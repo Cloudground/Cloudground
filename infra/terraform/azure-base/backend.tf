88d3b1418b81820dd337d77560e48fa54df24c4b
Michael Paesold <michael.paesold@nttdata.com>
2018-08-30 23:26:50 +0200

    Implement shared storage backend

--- infra/terraform/azure-base/backend.tf
+++ infra/terraform/azure-base/backend.tf
@@ -1,9 +1,5 @@
-/*
 terraform {
-  backend "azurerm" {
-    storage_account_name = "cloudgroundtfstate"
-    container_name = "base"
-    key = "base.terraform.tfstate"
-  }
+
+  backend "azurerm" {}
+
 }
-*/
---
+++ infra/terraform/azure-base/backend.tfvars
@@ -0,0 +1,7 @@
+resource_group_name = "cloudground-state"
+
+storage_account_name = "cloudgroundtfstate"
+
+container_name = "terraform-state"
+
+key = "base.terraform.tfstate"
