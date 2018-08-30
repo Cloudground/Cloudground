fc3b8e38db6c229d37071b109d622c01d57ab556
Michael Paesold <michael.paesold@nttdata.com>
2018-08-30 23:25:53 +0200

    Declare version dependencies
    Not strictly required but useful

--- infra/terraform/azure-base/main.tf
+++ infra/terraform/azure-base/main.tf
@@ -1,4 +1,12 @@
-provider "azurerm" {}
+terraform {
+  /* Make sure everyone uses a current version of terraform. */
+  required_version = ">= 0.11.8"
+}
+
+provider "azurerm" {
+  /* Avoid breaking changes by future versions of the provider */
+  version = "~> 1.13"
+}
 
 /* Azure resource group base infrastructure: container registry, etc.*/
 resource "azurerm_resource_group" "base_rg" {
